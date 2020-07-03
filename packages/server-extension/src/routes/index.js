3c8d5d7e57468fa26ee284628759177f4a026757
KadoBOT <ricardo.ambrogi@gmail.com>
2020-07-03 14:56:33 +0200

    fix middleware webhook validation

--- packages/server-extension/src/routes/index.js
+++ packages/server-extension/src/routes/index.js
@@ -2,6 +2,12 @@ import express from 'express'
 import payments from './payments/index'
 import paymentMethods from './paymentMethods/index'
 import originKeys from './originKeys/index'
+import errorMiddleware from '../middlewares/errorHandler'
+import validateWebhookMiddleware from '../middlewares/validateWebhook'
+import loggerMiddleware from '../middlewares/logger'
+import createClient from '../middlewares/createClient'
+import uaParser from '../middlewares/uaParser'
+import occClient from '../middlewares/occClient'
 import cache from '../helpers/serverCache'
 import clearCache from './clearCache/index'
 
@@ -9,10 +15,16 @@ const router = express.Router()
 const oneWeek = 604800
 const oneDay = 86400
 
-router.use('/v1/payments', payments)
+router.use(loggerMiddleware)
+router.use(uaParser)
+router.use(occClient, createClient)
+
+router.use('/v1/payments', validateWebhookMiddleware, payments)
 router.use('/v1/paymentMethods', cache(oneWeek), paymentMethods)
 router.use('/v1/originKeys', cache(oneDay), originKeys)
 router.use('/v1/clearCache', clearCache)
+
+router.use(errorMiddleware)
 
 const adyenRouter = router.use('/adyen', router)
 
